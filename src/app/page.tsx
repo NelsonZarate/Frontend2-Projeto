82581e8dd386321715301579916a17119a5c3b21
nelson <nelson_zarate@eticalgarve.com>
2026-02-18 20:34:16 +0000

    fix: navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,9 @@
-import GlitchText from "@/components/GlitchText";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="flex min-h-screen justify-center font-sans bg-background text-foreground">
-     <GlitchText
-                speed={4}
-                enableShadows
-                enableOnHover={false}
-                className="custom-class"
-              >
-                PartyFinder
-              </GlitchText>
-
-           <p className="font-display"> eu sou gamer </p>
+      
     </div>
   );
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { useState } from "react";
-import { Search, Bell, Menu, X } from "lucide-react";
+import { Search, Bell, Menu, X, LogOut } from "lucide-react";
 // import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import GlitchText from "./GlitchText";
@@ -34,28 +34,38 @@ const Navbar = () => {
             </li>
           ))}
         </ul>
-        <div className="font-bitcount text-xl">
+        <div className="font-bitcount text-xl flex gap-5 items-center">
           {isLoggedIn ? (
             <>
               <button className="relative text-muted-foreground hover:text-foreground transition-colors">
                 <Bell className="h-5 w-5" />
               </button>
-              <button
-                onClick={() => setIsLoggedIn(false)}
-                className="h-8 w-8 rounded-full bg-[var(--discord-blurple)] flex items-center justify-center text-xs font-bold text-foreground"
-              >
+              <button className="h-8 w-8 rounded-full bg-[var(--discord-blurple)] flex items-center justify-center text-xs font-bold text-foreground">
                 PF
               </button>
+              <button
+                onClick={() => setIsLoggedIn(false)}
+                className="flex items-center gap-2 bg-red-600 hover:bg-red-700 text-white px-4 py-1 rounded-lg transition-colors"
+              >
+                <LogOut size={20} />
+                <span>Logout</span>
+              </button>
             </>
-          ) : (<>
-            <button onClick={() => setIsLoggedIn(true)}
-              className="py-3 hidden md:inline-flex bg-[var(--discord-blurple)] hover:bg-[var(--discord-blurple-hover)] text-foreground gap-2 rounded-md px-3 py-1 text-sm font-medium transition-colors "
-            >  
-              <svg className="h-4 w-4" viewBox="0 0 24 24" fill="currentColor">
-                <path d="M20.317 4.37a19.791 19.791 0 0 0-4.885-1.515.074.074 0 0 0-.079.037c-.21.375-.444.864-.608 1.25a18.27 18.27 0 0 0-5.487 0 12.64 12.64 0 0 0-.617-1.25.077.077 0 0 0-.079-.037A19.736 19.736 0 0 0 3.677 4.37a.07.07 0 0 0-.032.027C.533 9.046-.32 13.58.099 18.057a.082.082 0 0 0 .031.057 19.9 19.9 0 0 0 5.993 3.03.078.078 0 0 0 .084-.028c.462-.63.874-1.295 1.226-1.994a.076.076 0 0 0-.041-.106 13.107 13.107 0 0 1-1.872-.892.077.077 0 0 1-.008-.128 10.2 10.2 0 0 0 .372-.292.074.074 0 0 1 .077-.01c3.928 1.793 8.18 1.793 12.062 0a.074.074 0 0 1 .078.01c.12.098.246.198.373.292a.077.077 0 0 1-.006.127 12.299 12.299 0 0 1-1.873.892.077.077 0 0 0-.041.107c.36.698.772 1.362 1.225 1.993a.076.076 0 0 0 .084.028 19.839 19.839 0 0 0 6.002-3.03.077.077 0 0 0 .032-.054c.5-5.177-.838-9.674-3.549-13.66a.061.061 0 0 0-.031-.03z" />
-              </svg>
-              Login with Discord
-            </button>
+          ) : (
+            <>
+              <button
+                onClick={() => setIsLoggedIn(true)}
+                className="hidden md:inline-flex bg-[var(--discord-blurple)] hover:bg-[var(--discord-blurple-hover)] text-foreground gap-2 rounded-md px-3 py-2 text-sm font-medium transition-colors "
+              >
+                <svg
+                  className="h-4 w-4"
+                  viewBox="0 0 24 24"
+                  fill="currentColor"
+                >
+                  <path d="M20.317 4.37a19.791 19.791 0 0 0-4.885-1.515.074.074 0 0 0-.079.037c-.21.375-.444.864-.608 1.25a18.27 18.27 0 0 0-5.487 0 12.64 12.64 0 0 0-.617-1.25.077.077 0 0 0-.079-.037A19.736 19.736 0 0 0 3.677 4.37a.07.07 0 0 0-.032.027C.533 9.046-.32 13.58.099 18.057a.082.082 0 0 0 .031.057 19.9 19.9 0 0 0 5.993 3.03.078.078 0 0 0 .084-.028c.462-.63.874-1.295 1.226-1.994a.076.076 0 0 0-.041-.106 13.107 13.107 0 0 1-1.872-.892.077.077 0 0 1-.008-.128 10.2 10.2 0 0 0 .372-.292.074.074 0 0 1 .077-.01c3.928 1.793 8.18 1.793 12.062 0a.074.074 0 0 1 .078.01c.12.098.246.198.373.292a.077.077 0 0 1-.006.127 12.299 12.299 0 0 1-1.873.892.077.077 0 0 0-.041.107c.36.698.772 1.362 1.225 1.993a.076.076 0 0 0 .084.028 19.839 19.839 0 0 0 6.002-3.03.077.077 0 0 0 .032-.054c.5-5.177-.838-9.674-3.549-13.66a.061.061 0 0 0-.031-.03z" />
+                </svg>
+                Login with Discord
+              </button>
             </>
           )}
         </div>
